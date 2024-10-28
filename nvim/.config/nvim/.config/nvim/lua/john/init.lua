d5a3a764893be15eff209150cdb946beba1fa41b
johndalloxx <john.dall@oxx.no>
2024-10-28 14:32:33 +0100

    cream-G

--- nvim/.config/nvim/.config/nvim/lua/john/init.lua
+++
@@ -1,7 +0,0 @@
-require('luasnip.loaders.from_vscode').lazy_load()
-require("john.remap")
-require("john.set")
-require("john.packer")
-require("john.theme")
-require('nvim-autopairs').setup({})
-require('hop').setup({})
